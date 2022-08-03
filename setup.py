30ef83908c446e441f35e56995cb58c93a99812d
Jared Kipe <jared@hibou.io>
2022-08-03 08:04:44 -0700

    Upgrade psycopg2-binary for modern TLS and password hashing

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ setup(
         'pillow',  # windows binary http://www.lfd.uci.edu/~gohlke/pythonlibs/
         'psutil',  # windows binary code.google.com/p/psutil/downloads/list
         'psycogreen',
-        'psycopg2 >= 2.2',
+        # 'psycopg2 >= 2.2',
         'python-chart',
         'pydot',
         'pyparsing',
